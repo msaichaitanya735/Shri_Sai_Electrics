db6c7643c5661b3d0c0c2f8e9eaa2617469f7528
msaichaitanya735 <43997513+msaichaitanya735@users.noreply.github.com>
2022-03-14 19:57:40 +0530

    mobileimages

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -10,14 +10,21 @@ import {Carousel} from '3d-react-carousal';
 import carousel1 from '../img/carousel1.svg';
 import carousel2 from '../img/carousel2.svg';
 import carousel3 from '../img/carousel3.svg';
-
+import mobcarousal from '../img/mob-car1.svg';
+import mobcarousal2 from '../img/mob-car2.svg';
+import mobcarousal3 from '../img/mob-car3.svg';
 
 const Homepage = () => {
-  let slides = [
+  let slides = width<600?[
     <img  src={carousel1} alt="1" />,
     <img  src={carousel2} alt="2" />  ,
     <img  src={carousel3} alt="3" />  ,
-  ];
+  ]:[<img  src={mobcarousal} alt="1" />,
+  <img  src={mobcarousal2} alt="2" />  ,
+  <img  src={mobcarousal3} alt="3" />  ,];
+
+  var width = window.innerWidth;
+  console.log(width);
   return (
     <div>
     {/* <HomeScreen /> */}
